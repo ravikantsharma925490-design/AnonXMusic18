aca6a0d55c5b2506e596243d6ab4a19683d50f4b
ravikantsharma925490-design <ravikantsharma925490@gmail.com>
2026-09-03 16:04:57 +0530

    Update youtube.py
    Signed-off-by: ravikantsharma925490-design <ravikantsharma925490@gmail.com>

--- anony/core/youtube.py
+++ anony/core/youtube.py
@@ -180,14 +180,16 @@ class YouTube:
             "logger": DummyLogger(),
             "nocheckcertificate": True,
             "cookiefile": cookie,
+            
+            # 🔥 BYPASS MULTIPLE LOGIC FOR EXTRACTOR FIX
             "extractor_args": {
                 "youtube": {
-                    "player_client": ["mweb", "ios"],
-                    "player_skip": ["configs", "webpage"],
+                    "player_client": ["web_safari", "ios", "mweb", "default"],
+                    "player_skip": ["webpage", "configs"]
                 }
             },
             "http_headers": {
-                "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 17_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Mobile/15E148 Safari/604.1",
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15",
                 "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
                 "Accept-Language": "en-US,en;q=0.9",
             }
