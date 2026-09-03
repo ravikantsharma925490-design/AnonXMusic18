2a3521d4944332facd2c93bb35ab076c02a418d6
ravikantsharma925490-design <ravikantsharma925490@gmail.com>
2026-09-03 17:01:41 +0530

    Update __init__.py
    Signed-off-by: ravikantsharma925490-design <ravikantsharma925490@gmail.com>

--- anony/__init__.py
+++ anony/__init__.py
@@ -2,6 +2,11 @@
 # Licensed under the MIT License.
 # This file is part of AnonXMusic
 
+from config import Config
+
+config = Config()
+config.check()
+
 from anony.core.bot import Bot
 from anony.core.dir import dirr
 from anony.core.git import git
