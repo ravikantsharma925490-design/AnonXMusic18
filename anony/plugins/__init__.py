9dfc4d590f87931a6079635a2ab2b1906aa1dac5
ravikantsharma925490-design <ravikantsharma925490@gmail.com>
2026-09-03 16:36:32 +0530

    Update __init__.py
    Signed-off-by: ravikantsharma925490-design <ravikantsharma925490@gmail.com>

--- anony/plugins/__init__.py
+++ anony/plugins/__init__.py
@@ -2,34 +2,24 @@
 # Licensed under the MIT License.
 # This file is part of AnonXMusic
 
-import logging
-from pyrogram import Client
-from .core.bot import Bot
-from .core.dir import dirr
-from .core.git import git
-from .core.userbot import Userbot
-from .misc import dbb, heroku, sudo
+from anony.core.bot import Bot
+from anony.core.dir import dirr
+from anony.core.git import git
+from anony.core.userbot import Userbot
+from anony.misc import dbb, heroku, sudo
 
-# 1. Initialize Loggers
-logging.basicConfig(
-    format="[%(asctime)s - %(levelname)s] - %(name)s: %(message)s",
-    level=logging.INFO,
-)
-logger = logging.getLogger("anony")
+from .logging import logger
 
-# 2. Directory and Environment configurations 
 dirr()
 git()
 dbb()
 heroku()
 sudo()
 
-# 3. Structural class declarations
 app = Bot()
 userbot = Userbot()
 
-# Explicit global class object linking to avoid client spec errors
-from .core.custom import Anon
-anon = Anon()
 
-__all__ = ["app", "userbot", "anon", "logger"]
+from .core.custom import Anon
+
+anon = Anon()
