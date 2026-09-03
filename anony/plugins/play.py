c3b33dfd98589eb8f506816e1ff3a2078c8caadf
ravikantsharma925490-design <ravikantsharma925490@gmail.com>
2026-09-03 16:41:26 +0530

    Update play.py
    Signed-off-by: ravikantsharma925490-design <ravikantsharma925490@gmail.com>

--- anony/plugins/play.py
+++ anony/plugins/play.py
@@ -10,7 +10,8 @@ import asyncio
 
 from pyrogram import filters, types
 
-from anony import anon, app, config, db, lang, queue, tg, yt
+# Safe global initialization mapping
+from anony import app, config, db, lang, queue, tg, yt
 from anony.helpers import buttons, utils
 from anony.helpers._play import checkUB
 
@@ -31,6 +32,7 @@ async def background_file_downloader(query: str, chat_id: int, user_id: int, mes
     os.makedirs(download_dir, exist_ok=True)
     
     try:
+        # Utilizing fallback safe pipeline stream flags
         command = f'spotdl download "{query}" --output "{download_dir}/%(title)s.%(ext)s" --format mp3 --use-sequential-workers'
         process = await asyncio.create_subprocess_shell(
             command,
@@ -275,6 +277,8 @@ async def play_hndlr(
                 )
 
     try:
+        # 🔥 FUNCTIONAL LAYER SAFE RUN: Import locally inside execution flow to break dependency loops
+        from anony import anon
 
         await anon.play_media(
             chat_id=m.chat.id,
